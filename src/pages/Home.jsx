1217875ef4a33babb7866ffd8d5152bd36e871ec
Daniel Willoughby <daniel.willoughby@connect.qut.edu.au>
2025-08-07 14:10:02 +1000

    fixed home invalid classNames

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,10 @@ function Home() {
   return (
     <>
       <Container className="py-4">
-        <div class="p-5 mb-4 bg-body-tertiary rounded-3">
+        <div className="p-5 mb-4 bg-body-tertiary rounded-3">
           <Container fluid className="p5-y">
-            <h1 class="display-5 fw-bold">Custom jumbotron</h1>
-            <p class="col-md-8 fs-4">
+            <h1 className="display-5 fw-bold">Custom jumbotron</h1>
+            <p className="col-md-8 fs-4">
               Using a series of utilities, you can create this jumbotron, just
               like the one in previous versions of Bootstrap. Check out the
               examples below for how you can remix and restyle it to your
